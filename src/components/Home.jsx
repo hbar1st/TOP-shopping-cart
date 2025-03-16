f5d22b17da62d5406f6dc82e067f0349c4249ba3
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-03-16 19:34:53 -0400

    fix: light theme nav color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -60,7 +60,7 @@ function Home() {
       <NavLink to="/shop">
         <div
           aria-label="slideshow"
-          aria-live="false"
+          aria-live="off"
           aria-description="Hanazon poster slideshow"
           className="poster-frame"
           ref={posterSliderRef}
